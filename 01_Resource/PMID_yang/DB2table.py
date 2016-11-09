8987ccc02c36b14dbf190c0ecfe1642c6716263f
wzl <wzlnot@gmail.com>
2016-11-09 04:51:34 -0800

    fix

--- 01_Resource/PMID_yang/DB2table.py
+++ 01_Resource/PMID_yang/DB2table.py
@@ -37,7 +37,7 @@ def query_pmid(DBSession):
             ab_text = ab.abstract_text
         except:
             ab_text = None
-        tit = query_session.query(PubMedDB.Citation.article_title).filter(
+        tit = query_session.query(PubMedDB.Citation).filter(
                                   PubMedDB.Citation.pmid == pmid).one()
         try:
             auth = query_session.query(PubMedDB.Author).filter(
@@ -45,17 +45,20 @@ def query_pmid(DBSession):
             authlist = [' '.join([i.last_name, i.fore_name]) for i in auth]
         except:
             authlist = None
-        output[pmid] = {'title': tit[0],
-                        'j_issn': s.issn,
-                        'j_volume': s.volume,
-                        'j_issue': s.issue,
-                        'j_pud_date_year': s.pub_date_year,
-                        'j_pub_date_month': s.pub_date_month,
-                        'j_pub_date_day': s.pub_date_day,
-                        'j_title': s.title,
-                        'j_iso_abbr': s.iso_abbreviation,
-                        'abstract': ab_text,
-                        'author_list': authlist}
+        output[pmid] = {'title': tit.article_title,  # 文章.标题(isNull=False)
+                        'j_start_page': tit.start_page,  # 杂志.起始页码
+                        'j_end_page': tit.end_page,  # 杂志.终止页码
+                        'j_medline_pgn': tit.medline_pgn,  # 杂志.缩略页码
+                        'j_issn': s.issn,  # 杂志.issn号
+                        'j_volume': s.volume,  # 杂志.卷号
+                        'j_issue': s.issue,  # 杂志.期号
+                        'j_pud_date_year': s.pub_date_year,  # 杂志.出版年(isNull=False)
+                        'j_pub_date_month': s.pub_date_month,  # 杂志.出版月
+                        'j_pub_date_day': s.pub_date_day,  # 杂志.出版日
+                        'j_title': s.title,  # 杂志.全名
+                        'j_iso_abbr': s.iso_abbreviation,  # 杂志.缩略名
+                        'abstract': ab_text,  # 文章.摘要
+                        'author_list': authlist}  # 文章.作者（列表）
     query_session.close
     return output
 
